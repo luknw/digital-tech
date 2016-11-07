8d59b5da9204e9f9b22609faf2bef51addc78189
Marcin Przewięźlikowski <m.przewie@gmail.com>
2016-11-07 02:54:11 +0100

    lab2 nearly done

--- Karnaugh/KarnaughTab.py
+++ Karnaugh/KarnaughTab.py
@@ -2,12 +2,12 @@ import csv
 import xlsxwriter
 
 def karnaugh(seg):
-    array = [['x', bin(0), bin(1), bin(3), bin(2)]]
+    array = [['ab/cd', bin(0)[2:].zfill(2), bin(1)[2:].zfill(2), bin(3)[2:].zfill(2), bin(2)[2:].zfill(2)]]
     for i in range(0, 4):
         arr = []
         for j in range(0, 4):
-            arr.append(seg((4 * i + j) % 10))
-        appendix = [bin(i)] + arr
+            arr.append(seg((4 * i + j)))
+        appendix = [bin(i)[2:].zfill(2)] + arr
         array.append(appendix)
     tmp = array[4]
     array[4] = array[3]
@@ -18,42 +18,49 @@ def karnaugh(seg):
     return array
 
 def seg0(num):
+    if(num>9): return "x"
     if num in [0, 2, 3, 6, 7, 8, 9]:
         return 1
     else:
         return 0
 
 def seg1(num):
+    if (num > 9): return "x"
     if num in [0, 4, 5, 6, 8, 9]:
         return 1
     else:
         return 0
 
 def seg2(num):
+    if (num > 9): return "x"
     if num in [0, 1, 2, 3, 4, 7, 8, 9]:
         return 1
     else:
         return 0
 
 def seg3(num):
+    if (num > 9): return "x"
     if num in [2, 3, 4, 5, 6, 8, 9]:
         return 1
     else:
         return 0
 
 def seg4(num):
+    if (num > 9): return "x"
     if num in [0, 2, 6, 8]:
         return 1
     else:
         return 0
 
 def seg5(num):
+    if (num > 9): return "x"
     if num in [0, 1, 3, 4, 5, 6, 7, 8, 9]:
         return 1
     else:
         return 0
 
 def seg6(num):
+    if (num > 9): return "x"
     if num in [0, 2, 3, 5, 6, 8, 9]:
         return 1
     else:
@@ -80,6 +87,10 @@ for s in range (0,7):
 
 workbook.close()
 
+for i in range(0,7):
+    print("seg"+str(i)+":")
+    for j in range(0,10):
+        print(str(j)+" -> "+str(segs[i](j)))
 
 
 
